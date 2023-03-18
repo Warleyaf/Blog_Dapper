828f6b3fc41a4df48107225e63da2fb7668bd125
Warley Afonso <warleysilva365@gmail.com>
2023-03-17 21:33:50 -0300

    Criando a parte de lista de roles na minha classe do User

--- Blog/Models/Category.cs
+++ Blog/Models/Category.cs
@@ -8,5 +8,7 @@ namespace Blog.Models
       public int Id { get; set; }
       public string Name { get; set; }
       public string Slug { get; set; }
+      public List<Post> Posts { get; set; } // Eu teria aqui uma propriedade de listas de posts chamada de "Posts";
+      // Ou seja no "Category" eu teria uma lista de Posts
    }
 }
--- Blog/Models/User.cs
+++ Blog/Models/User.cs
@@ -13,5 +13,12 @@ namespace Blog.Models
         public string Bio { get; set; }
         public string Image { get; set; }
         public string Slug { get; set; }
+        public List<Role> Roles { get; set; } // aqui eu vou ter uma lista de Roles, ou seja, no meu usuário eu posso ter vários Roles
+
+        public User() {
+            Roles = new List<Role>(); // Por boa prática é bom a gente já vir e criar um método construtor, e nesse construtor instanciar essa lista, isso é para nunca acontecer dele tentar incluir um role e essa lista estar vazia
+        }
     }
+
+   
 }
